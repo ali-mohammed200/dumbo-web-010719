be5d40044d342f1dd9f25deaea694d469a32d511
ali-mohammed200 <ali.mohammed200@gmail.com>
2019-03-06 10:29:24 -0500

    finished

--- 24-debugging-workshop/src/index.js
+++ 24-debugging-workshop/src/index.js
@@ -1,4 +1,5 @@
 function handleSubmit(e) {
+  event.preventDefault()
   inputString = e.target[0].value
   identicon = new Identicon(inputString);
   updateGravatar(identicon)
@@ -19,13 +20,13 @@ function newComment(comment, gravatar) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Accept: 'application/json'
+      'Accept': 'application/json'
     },
     body: JSON.stringify({
       content: comment,
       gravatar: gravatar
     })
-  })
+  }).then(res => res.json()).then(json => loadComments(json.gravatar))
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -40,9 +41,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const gravatar = document.getElementById("identicon-form")[0].value
 
   commentForm.addEventListener("submit", (event) => {
-    const comment = document.querySelector('#comment-form > div > input[type="text"]')
+    event.preventDefault()
+    const comment = document.querySelector('#comment-form > div > input[type="text"]').value
     const gravatar = document.getElementById("identicon-form")[0].value
+    console.log(comment)
     newComment(comment, gravatar)
-    loadComments(gravatar)
   })
 })
